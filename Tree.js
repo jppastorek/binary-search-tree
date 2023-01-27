81775912d4e0ff26a29dc20baa289aa464af8ecd
jppastorek <jppastorek@gmail.com>
2023-01-26 22:46:06 -0600

    Begun delete function. Recursion complete

--- Tree.js
+++ Tree.js
@@ -33,12 +33,13 @@ export default class Tree {
     if (node.left !== null) {
       this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
     }
-  }
+  };
 
-  find(value, root) { //cannot use root because it wont update to be current if recursive
+  find(value, root) {
+    //cannot use root because it wont update to be current if recursive
     if (root == null) return null;
     if (value == root.data) return root; //exit statement --> if the value is equal to current node
-    
+
     //recursively check each mini tree's mid point
     if (value < root.data) return this.find(value, root.left);
     if (value > root.data) return this.find(value, root.right);
@@ -49,23 +50,55 @@ export default class Tree {
     if (root == null) {
       root = new Node(value);
       return root;
-    };
+    }
 
     if (root.left == null && root.right == null) {
       if (value < root.data) {
         root.left = new Node(value);
-      }else {
+      } else {
         root.right = new Node(value);
       }
       return root;
     }
-
 
     //find the appropriate leaf
     if (value < root.data) return this.insert(value, root.left);
     if (value > root.data) return this.insert(value, root.right);
     //place the node on the appropriate side
     //return the new node
+  }
 
+  delete(value, root) {
+    //base case if root is null, return root
+    if (root == null) return root;
+
+    if (value < root.data) {
+      root.left = this.delete(value, root.left);
+    } else if (value > root.data) {
+      root.right = this.delete(value, root.right);
+    } else {
+      //leaf, just remove...one child copy to parent and delete child
+      //TODO  it recurses to the correct value, but something strange happens. when pretty print staerts again
+      //i notice that the first node's right is now undefined.
+      if (root.left == null) {
+        return root.right;
+      } else if (root.right == null) {
+        return root.left;
+      } else {
+        //two children, copy inorder successor (smallest right) and then delete inorder child (only if right child not empty)
+        root.data = inOrder(root.right);
+        root.right = this.delete(root.data, root.right);
+      }
+      return root;
+    }
+
+    function inOrder(root) {
+      let inOrderChild = root.data;
+      while (root.left != null) {
+        inOrderChild = root.left.data;
+        root = root.left;
+      }
+      return inOrderChild;
+    }
   }
 }
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import Tree from './Tree.js'
 
-const array = Array.from({length: 10}, () => Math.floor(Math.random() * 200));
+const array = Array.from({length: 20}, () => Math.floor(Math.random() * 200));
 const array2 = [1,2,3,4,5,6,7,8];
 
 let tree = new Tree(array2);
+tree.prettyPrint(tree.root);
 
-tree.insert(9, tree.root);
 tree.insert(-1, tree.root);
+tree.delete(8, tree.root);
 
 
 tree.prettyPrint(tree.root);
